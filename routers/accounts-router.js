a36725ff360f276eba2d5e64a78c8b19ae61b617
mlazzell17@gmail.com <MicheSi>
2020-02-17 13:49:49 -0700

    able to do get requests to accounts and accounts/:id

--- routers/accounts-router.js
+++ routers/accounts-router.js
@@ -5,8 +5,30 @@ const db = require('../data/dbConfig');
 const router = express.Router();
 
 // get all accounts
+router.get('/', (req, res) => {
+    db.select('*').from('accounts')
+    .then(accounts => {
+        res.status(200).json(accounts)
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({error: err.message})
+    })
+})
 
 // get account by id
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    db('accounts').where({id})
+    .first()
+    .then(account => {
+        res.status(200).json(account)
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({error: err.message})
+    })
+})
 
 // add new account
 
