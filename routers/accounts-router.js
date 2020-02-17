7f3c1c1841f79d50a14a7be7337c60b14eef6fe0
mlazzell17@gmail.com <MicheSi>
2020-02-17 13:39:36 -0700

    server running on 4000

---
+++ routers/accounts-router.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const db = require('../data/dbConfig');
+
+const router = express.Router();
+
+// get all accounts
+
+// get account by id
+
+// add new account
+
+// update account
+
+// delete account
+
+module.exports = router;
